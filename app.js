5d44e433c3579674e7e8ea95a69fd649a0b43be0
HowNLuo <xji6cl4p@gmail.com>
2021-07-15 15:03:45 +0800

    feat: add handlebars & modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
 
 const mongoose = require('mongoose')
 const db = mongoose.connection
@@ -15,8 +16,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
 })
 
 app.listen(port, () => {
